985615efef3ac31850e487584e8be1576b86d584
Anton Morgunov <morgunov@mit.edu>
2023-04-02 17:17:05 -0400

    FEAT: correlations for 2021-2022

--- analysis/results_heat.py
+++ analysis/results_heat.py
@@ -6,9 +6,10 @@ import tools
 
 class Analyzer:
 
-    def __init__(self):
+    def __init__(self, year):
         self.ROUND3 = tools.rounder(3)
-        self.bygrade = parse_results(2023)
+        self.year = year
+        self.bygrade = parse_results(year)
 
     def results_for_problem(self, data, probnum, scale=100):
         return [getattr(obj, f"problem{probnum}")["rel"]*scale for obj in data]
@@ -25,9 +26,9 @@ class Analyzer:
         ffit = [[f"{self.ROUND3(arr[0])}x+{self.ROUND3(arr[1])}" for arr in row] for row in bestfit]
         labels = [f"№{i}" for i in range(1, NUMPROB+1)]
         traces = [go.Heatmap(z=corrs, text=cortext, texttemplate="%{text}", x=labels, y=labels, colorscale='dense')]
-        graph.plot_heatmap(traces, f"results/2023-grade{grade}", f"Корреляция между задачами ({grade} кл.)")
+        graph.plot_heatmap(traces, f"results/{self.year}/grade{grade}", f"Корреляция между задачами ({grade} кл.)")
         traces = [go.Heatmap(z=corrs, text=ffit, texttemplate="%{text}", x=labels, y=labels, colorscale='dense')]
-        graph.plot_heatmap(traces, f"results/2023-grade{grade}-fit", f"Корреляция между задачами ({grade} кл.)")
+        graph.plot_heatmap(traces, f"results/{self.year}/grade{grade}-fit", f"Корреляция между задачами ({grade} кл.)")
 
     def line_two_problems(self, grade, i, j):
         graph = Graph()
@@ -41,20 +42,28 @@ class Analyzer:
         yfit = np.vectorize(lambda x: fit[0]*x+fit[1])(xgrid)
         traces = [go.Scatter(x=x, y=y, mode='markers', name="Результаты"),
                   go.Scatter(x=xgrid, y=yfit, mode='lines', name="Best fit")]
-        graph.plot_linechart(traces, f"results/2023-grade{grade}-{i}-{j}", f"Корреляция между задачами {i} и {j}", f"Задача №{i}", f"Задача №{j}", eq)
+        graph.plot_linechart(traces, f"results/{self.year}/grade{grade}-{i}-{j}", f"Корреляция между задачами {i} и {j}", f"Задача №{i}", f"Задача №{j}", eq)
 
 
 if __name__ == "__main__":
-    a = Analyzer()
+    # a = Analyzer(2023)
+    # for grade in (9, 10, 11):
+    #     a.heat_map_for_grade(grade)
+    # a.line_two_problems(11, 2, 8)
+    # a.line_two_problems(11, 2, 7)
+    # a.line_two_problems(11, 3, 8)
+    # a.line_two_problems(11, 4, 5)
+    # a.line_two_problems(11, 5, 6)
+    # a.line_two_problems(10, 2, 5)
+    # a.line_two_problems(10, 4, 5)
+    # a.line_two_problems(10, 4, 6)
+    # a.line_two_problems(10, 4, 7)
+    # a.line_two_problems(10, 5, 6)
+
+    a = Analyzer(2022)
     for grade in (9, 10, 11):
         a.heat_map_for_grade(grade)
-    a.line_two_problems(11, 2, 8)
-    a.line_two_problems(11, 2, 7)
-    a.line_two_problems(11, 3, 8)
-    a.line_two_problems(11, 4, 5)
-    a.line_two_problems(11, 5, 6)
-    a.line_two_problems(10, 2, 5)
-    a.line_two_problems(10, 4, 5)
-    a.line_two_problems(10, 4, 6)
-    a.line_two_problems(10, 4, 7)
-    a.line_two_problems(10, 5, 6)
+
+    a = Analyzer(2021)
+    for grade in (9, 10, 11):
+        a.heat_map_for_grade(grade)
